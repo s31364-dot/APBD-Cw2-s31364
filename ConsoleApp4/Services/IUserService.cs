a3249fdc3ad435275dd9022102b8958d474e1046
Anton Zviahintsev <s31364@pjwstk.edu.pl>
2026-03-23 18:13:46 +0100

    Dodanie warstwy serwisowej dla użytkowników

---
+++ ConsoleApp4/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleApp4.Users;
+
+namespace ConsoleApp4.Services;
+
+public interface IUserService
+{
+    void AddUser(User user);
+}
---
+++ ConsoleApp4/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp4.Users;
+
+namespace ConsoleApp4.Services;
+
+public class UserService : IUserService
+{
+    private readonly List<User> _users = new();
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+}
