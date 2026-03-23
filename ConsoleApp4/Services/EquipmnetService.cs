df49eac1d972bf23dc98b1ea39f5c062e4cb7b7b
Anton Zviahintsev <s31364@pjwstk.edu.pl>
2026-03-23 18:42:13 +0100

    Implementacja EquipmentService z logiką filtrowania

---
+++ ConsoleApp4/Services/EquipmnetService.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp4.Equipment;
+using ConsoleApp4.Enums;
+using ConsoleApp4.Users;
+
+namespace ConsoleApp4.Services;
+using Equipment = ConsoleApp4.Equipment.Equipment;
+
+public class EquipmnetService
+{
+    readonly List<Equipment> _equipmentList = new();
+    
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipmentList.Add(equipment);
+        Console.WriteLine($"Dodano sprzęt: {equipment.Name} (ID: {equipment.Id})");
+    }
+    
+    public List<Equipment> GetAllEquipment()
+    {
+        return _equipmentList;
+    }
+
+    public List<Equipment> GetAvailableEquipment()
+    {
+        return _equipmentList
+            .Where(e => e.Status == EquipmentStatus.Avalible)
+            .ToList();
+    }
+
+}
---
+++ ConsoleApp4/Services/IEquipmentService.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp4.Equipment;
+
+
+namespace ConsoleApp4.Services;
+using Equipment = ConsoleApp4.Equipment.Equipment;
+public interface IEquipmentService
+{
+    void addEquipment();
+    
+    List<Equipment> GetAllEquipment();
+    
+    List<Equipment> GetAvailableEquipment();
+    
+}
